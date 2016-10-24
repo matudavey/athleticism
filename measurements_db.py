0c4acdad6a64064b822f5f532e9b19f3a6bc74ec
matudavey <matudavey@hotmail.com>
2016-10-24 23:44:45 +0100

    added python teest script to add values and gt data from db

---
+++ measurements_db.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 24 22:18:29 2016
+
+@author: Matthew
+"""
+
+import pyodbc
+from sqlalchemy import create_engine
+import pandas as pd
+from matplotlib import pyplot as plt
+class User:
+    def __init__(self,username,pswd):
+        if username == 'Matt' and pswd == 'pass':
+            self.uid = 22
+        else: print 'Bad Login'
+        
+def add_meas(meas_date,user_cfg,meas,value,units):
+    engine = create_engine('mssql+pyodbc://teamsolveig:q02XxfG2@athleticism.database.windows.net:1433/measurements', echo=True)
+    cols = ['meas_date','user_id','measurement', 'meas_value', 'meas_units_id']
+    x = [meas_date,user_cfg.uid,meas,value,units]
+    
+    df = pd.DataFrame([x],columns = cols)
+    df = df.set_index('meas_date')
+    print df
+    df.to_sql('meas',engine, if_exists = 'append')
+    
+def get_mass(uid):
+    query = """ SELECT meas_date, meas_value FROM [measurements].[dbo].[meas] WHERE user_id = %d""" % uid
+    engine = create_engine('mssql+pyodbc://teamsolveig:q02XxfG2@athleticism.database.windows.net:1433/measurements', echo=True)
+    df = pd.read_sql(query,engine)
+    df = df.set_index('meas_date')
+    return df
+    
+if __name__ == '__main__':
+    meas_date = None
+    units = 1
+    meas = 1
+    value = 77.8
+    user = User('Matt','pass')
+    
+    if False: # add new mass value
+        add_meas(meas_date,user,meas,value,units)
+        
+     if False: #plot mass vs time       
+        mass_df = get_mass(user.uid)
+        print mass_df
+        mass_df.meas_value.plot()
+        plt.show()
+        plt.close()
+    
